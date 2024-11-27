c46e0da683db48ff4214a786d8b59b7abf1cbf18
olegbom <olegbom@inbox.ru>
2024-11-27 20:15:10 +0300

    add linux support

--- src/main.c
+++ src/main.c
@@ -1,25 +1,69 @@
 #include <stdio.h>
 #include "field.h"
-#include <conio.h>
 
+#ifdef _WIN32
 #include <fcntl.h>
 #include <io.h>
 #include <windows.h>
 
-__attribute__((constructor))
-void
-winsane_init(void)
+int getch()
 {
-    _setmode(0, _O_BINARY);
-    _setmode(1, _O_BINARY);
-    SetConsoleCP(CP_UTF8);  // maybe will work someday
-    SetConsoleOutputCP(CP_UTF8);
+    return _getch();
+}
+#endif
+
+#ifdef __linux__
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
 }
 
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+
+#endif
 
 int main()
 {
-    winsane_init();
 
     field_s f;
     if( !fieldTryInit( &f, 12 ) )
@@ -34,7 +78,7 @@ int main()
     cell_e chip = CELL_PLAYER_1;
     while(true)
     {
-        int c = _getch();
+        int c = getch();
         if( c == 'a' )
         {
             if( cursor_pos > 0 )
